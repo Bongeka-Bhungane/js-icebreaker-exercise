93b330ceeb93d7c42479484a49c54af8bec63241
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-08-08 12:05:47 +0200

    Add Partner 1 variables

---
+++ icebreaker.js
@@ -0,0 +1,9 @@
+const favMovie = "Wednesday";
+console.log("My favourite movie is "+ favMovie + ".");
+
+let discountCode = "";
+console.log("User discount code is curently " + discountCode + " empty.");
+
+let profilePictureURL = null;
+console.log("your user profile picture is " + profilePictureURL);
+
