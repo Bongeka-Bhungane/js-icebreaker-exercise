55244bdaf4d4d9865784565b5d65e01b31160d17
Mluleki <melokuhlemluleki02@gmail.com>
2025-08-08 12:47:01 +0200

    Add Partner 2 variables

--- icebreaker.js
+++ icebreaker.js
@@ -1,9 +1,23 @@
 const favMovie = "Wednesday";
 console.log("My favourite movie is "+ favMovie + ".");
 
+let numberOfSiblings = 1;
+console.log("i have " + numberOfSiblings + " sibling.");
+
+let seenInception = false;
+console.log("Have i seen inception" + seenInception);
+
+let dreamVacation = {country: "France", city: "Paris", plannedActivity: "Watch a soccer match"};
+console.log("My dream vacation will be at " + dreamVacation.country + dreamVacation.city + "i want to " + dreamVacation.plannedActivity + ".");
+
+let favouriteFood = ["Swarma", "Burger", "Pizza"];
+console.log("my favorite food is " + favouriteFood.join(",") + ".");
+
 let discountCode = "";
 console.log("User discount code is curently " + discountCode + " empty.");
 
 let profilePictureURL = null;
 console.log("your user profile picture is " + profilePictureURL);
 
+let galacticStarID = 987654321098765432109876543210n;
+console.log("The galentic star ID is " + galacticStarID + ".");
